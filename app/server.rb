787152cdbdb2734eb67e7b0d192fabc095c6b8bb
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-04 10:13:59 -0500

    Los sockets solo envia mensajes a los usuarios suscritos al sensor

--- app/server.rb
+++ app/server.rb
@@ -6,7 +6,7 @@ require 'mongolitedb'
 require 'json'
 require_relative 'database'
 
-@clients = []
+@clients = [] #[ws faye, id_usuario, id_sensor] ...
 @i = 0
 
 App = lambda do |env|
@@ -14,22 +14,29 @@ App = lambda do |env|
     ws = Faye::WebSocket.new(env)
     db = Database.new
     query_params = QueryString.parse(env['QUERY_STRING'])
+    id_usuario = nil
+    id_sensor = nil
 
     ws.on :open do |event|
         rs = db.connection().find({'id_sensor' => query_params['id_sensor'][0]})
-        if rs.length >= 1
+        if rs.length >= 1 # el sensor ya se encuentra registrado en la base de datos
             id_usuarios = rs[0]['id_usuario']
-            if id_usuarios.include?(query_params['id_usuario'][0]) != true 
+            if id_usuarios.include?(query_params['id_usuario'][0]) != true # actualizar hash que relaciona un sensor con uno o muchos usuarios
                 id_usuarios.push(query_params['id_usuario'][0])
                 doc = {'id_sensor' => query_params['id_sensor'][0], 'id_usuario' => id_usuarios}
                 db.connection().update({'id_sensor' => query_params['id_sensor'][0]}, doc)
-            end
-        else
+            end 
+            # si el usuario ya se encunetra al sensor, no hace ya ningua operacion contra la base de datos que asocia ambos datos
+            id_usuario = query_params['id_usuario'][0]
+            id_sensor = query_params['id_sensor'][0]
+        else # dado que es un nuevo sensor, hay que inicializar un hash que relacione a este sensor con este primer usuario
             doc = {'id_sensor' => query_params['id_sensor'][0], 'id_usuario' => [query_params['id_usuario'][0]]}
             db.connection().insert(doc)
+            id_usuario = query_params['id_usuario'][0]
+            id_sensor = query_params['id_sensor'][0]
         end
 
-        @clients.push(ws)
+        @clients.push([ws, id_usuario, id_sensor])#[ws faye, id_usuario, id_sensor] ...
 
         p [:open]
         ws.send('Hello, world!')
@@ -37,9 +44,15 @@ App = lambda do |env|
 
     ws.on :message do |event|
         p [:on_message]
+        rs = db.connection().find({'id_sensor' => query_params['id_sensor'][0]})
+        id_usuarios = rs[0]['id_usuario']
         @clients.each do |client|
-            data_hash = JSON.parse(event.data)
-            client.send(data_hash['mensaje'])
+            ws_faye = client[0]
+            id_usuario = client[1]
+            if id_usuarios.include?(id_usuario) == true 
+                data_hash = JSON.parse(event.data)
+                ws_faye.send(data_hash['mensaje']) # sólo envía mensajes a las instacias de sockets que tienen asociado el usuario que se quiere conectar al sensor
+            end
         end
     end
 
